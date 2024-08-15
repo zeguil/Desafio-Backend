0c0b8e98bbea6f07d95f8abb33f82a889924b9c7
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2024-08-15 15:22:46 -0400

    add metodo GET e POST

--- tasks/models.py
+++ tasks/models.py
@@ -4,8 +4,8 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     due_date = models.DateField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def view_tasks(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
