86dea30b23ff7e29bd7a595229537f3088c7d68f
José Guilherme Lins Filho <jose.filho@elecnor.com.br>
2024-08-16 07:32:25 -0400

    add is_deleted no models

--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     due_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)  
 
     def __str__(self):
         return self.title
